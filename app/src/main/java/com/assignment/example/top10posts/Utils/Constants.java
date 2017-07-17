8139b4b0ae09cb123543ad44fad7b89d25ac9304
KarelDelgado <karel.delgado88@gmail.com>
2017-07-17 10:50:21 -0400

    Removing client_id, client_secret and redirect_uri

--- app/src/main/java/com/assignment/example/top10posts/Utils/Constants.java
+++ app/src/main/java/com/assignment/example/top10posts/Utils/Constants.java
@@ -8,9 +8,9 @@ public class Constants {
 
     public static final String TAG  = "Top10";
 
-    public static final String CLIENT_ID  = "1fa71cd70fd348eca2a60dcad8f2ba49";
-    public static final String CLIENT_SECRET  = "d8bbc2696cd64bb790432b3bab0014fb";
-    public static final String REDIRECT_URI  = "https://localhost";
+    public static final String CLIENT_ID  = "your_client_id";
+    public static final String CLIENT_SECRET  = "your_client_secret";
+    public static final String REDIRECT_URI  = "your_redirect_uri";
     public static final String GRANT_TYPE  = "authorization_code";
     public static final String RESPONSE_TYPE  = "code";
     public static final String PUBLIC_CONTENT_SCOPE  = "public_content";
